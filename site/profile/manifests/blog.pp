3c551aec773b5d6b88dc4046258e94c0f0b0e549
Gabriel M. Schuyler <gabe@puppet.com>
2018-09-20 14:12:28 -0700

    Do the apache part (hopefully)

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -2,6 +2,14 @@
 
 class profile::blog {
 
-
+  # Manage apache with mod_php
+  include apache
+  include apache::mod::php
+  
+  # Manage mysql with php bindings
+  
+  # Install wordpress somewhere that apache can see it
+  
+  
 
 }
